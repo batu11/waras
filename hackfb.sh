3389734e42dc22a1d29939d39f0e9383bfd751d5
Anak <56374392+batu11@users.noreply.github.com>
2019-10-10 02:05:21 +0000

    Hackfb.sh

---
+++ hackfb.sh
@@ -0,0 +1,12 @@
+inport requests
+
+
+def login(id,pw):
+    data ={'email':id,'pass':pw}
+    r =requests.post('https://basic.facebook.com/login',data=data
+    print(r.url)
+    
+if __name__=='__main__':
+     id =input('unsername:')
+     pw =input('password:')     
+     login(id,pw)
